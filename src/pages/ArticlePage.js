e356bb4c7ef9c62cabd11c8e730cf63b257d76ae
scottmo223 <scottmo223@yahoo.com>
2023-05-17 16:29:14 -0500

    upvotes connected to db

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,23 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
 import articles from './article-content'
 import NotFoundPage from './NotFoundPage'
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
     const { articleId } = useParams()
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`)
+            const newArticleInfo = response.data
+            setArticleInfo(newArticleInfo)
+        }
+
+        loadArticleInfo()
+    }, [])
+
     const article = articles.find((article) => article.name === articleId)
 
     if (!article) {
@@ -13,6 +27,7 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvotes</p>
             {article.content.map((paragraph, index) => (
                 <p key={index}>{paragraph}</p>
             ))}
